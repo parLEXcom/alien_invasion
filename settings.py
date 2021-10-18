3f772f5b18202246f9ba5a598a18db685fc48e33
lex32 <lex327@yandex.ru>
2021-10-18 21:23:41 +0300

    Обновление подщет очков 308

--- settings.py
+++ settings.py
@@ -27,6 +27,8 @@ class Settings():
 
         # Темп ускорения игры
         self.speedup_scale = 1.5
+        # Темп роста стоимости пришельцев
+        self.score_scale = 1.5
         self.initialize_dynamic_settings()
 
     def initialize_dynamic_settings(self):
@@ -41,9 +43,12 @@ class Settings():
         self.alien_points = 50
 
     def increase_speed(self):
-        """Увеличивает настройки скорости."""
+        """Увеличивает настройки скорости и стоимость пришельцев."""
         self.ship_speed_factor *= self.speedup_scale
         self.bullet_speed_factor *= self.speedup_scale
         self.alien_speed_factor *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
+
 
 
